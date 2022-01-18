8bb6f7c8f153197ab9fe5c3ff114278d3eea73a6
Orozdaan <Orozdaan19@gmail.com>
2022-01-18 20:08:04 +0600

    second version

---
+++ src/Expenses/Expenses.jsx
@@ -0,0 +1,10 @@
+import ExpenseForm from "./ExpenseForm"
+import './Expenses.css'
+const Expenses=()=>{
+    return(
+        <div className="new-expense">
+            <ExpenseForm/>
+        </div>
+    )
+}
+export default Expenses
